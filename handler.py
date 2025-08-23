5b03525de5d3880def4a80b7a5ed0367ed9651ea
nikhilbanthiya <nikhil.banthiya@gmail.com>
2025-08-23 16:02:06 +0530

    Simplify production server to standard WSGI with gthread

--- handler.py
+++ handler.py
@@ -25,7 +25,7 @@ import httpx
 
 from flask import Flask, request, jsonify, send_from_directory, Response, stream_with_context
 from flask_caching import Cache
-from a2wsgi import ASGIMiddleware
+# from a2wsgi import ASGIMiddleware
 
 from flask_cors import CORS
 import pandas as pd
@@ -1341,4 +1341,4 @@ def analyze():
 #    app.run(host='0.0.0.0', port=8000, debug=True)
 
 # Wrap the WSGI app in ASGI middleware for Uvicorn
-app = ASGIMiddleware(app)
+# app = ASGIMiddleware(app)
